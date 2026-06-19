b4dc379a64780c9101cbd9ce81f1bd47e886a6a0
Eman Abdulsemed <emanabdulsemed4398@gmail.com>
2026-06-19 11:37:59 +0300

    Time: 23 ms (75.75%), Space: 20.7 MB (61.2%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        counts = Counter(nums)
+        max_length = 0
+        
+        for num in counts:
+            if num + 1 in counts:
+                max_length = max(max_length, counts[num] + counts[num + 1])
+                
+        return max_length
