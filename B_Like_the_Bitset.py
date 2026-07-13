d0027e8ea86b4ba0d6ed148f4e83649570e84e4d
Eman Abdulsemed <emanabdulsemed4398@gmail.com>
2026-07-13 18:34:01 +0300

    Add files via upload

---
+++ B_Like_the_Bitset.py
@@ -0,0 +1,45 @@
+import sys
+
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    s = input().strip()
+
+    cnt = 0
+    ok = True
+
+    for ch in s:
+        if ch == '1':
+            cnt += 1
+            if cnt >= k:
+                ok = False
+                break
+        else:
+            cnt = 0
+
+    if not ok:
+        print("NO")
+        continue
+
+    print("YES")
+
+    ans = [0] * n
+    lo = 1
+    hi = n
+
+    # assign largest values to 0's
+    for i in range(n):
+        if s[i] == '0':
+            ans[i] = hi
+            hi -= 1
+
+    # assign smallest values to 1's
+    for i in range(n):
+        if s[i] == '1':
+            ans[i] = lo
+            lo += 1
+
+    print(*ans)
