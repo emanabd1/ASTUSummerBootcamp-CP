bdc23055d38862da6cb4e25f9f848f4101ee3d97
Eman Abdulsemed <emanabdulsemed4398@gmail.com>
2026-07-14 20:12:25 +0300

    B_Cake_Leveling.py

--- B_Cake_Leveling.py
+++ B_Cake_Leveling.py
@@ -15,7 +15,7 @@ def solve():
         n = int(data[idx])
         idx += 1
         
-        # Read the frosting heights for the current test case
+        
         a = [int(x) for x in data[idx : idx + n]]
         idx += n
         
@@ -25,10 +25,9 @@ def solve():
         
         for i in range(n):
             current_sum += a[i]
-            # The maximum possible height if we only considered prefix up to i
             max_h_at_i = current_sum // (i + 1)
             
-            # The actual height is constrained by the bottleneck of all prefixes so far
+            
             if max_h_at_i < ans:
                 ans = max_h_at_i
                 
