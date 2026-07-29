8d8ebb2b865a3e8ceeeb6f3114125e8b927b7b8d
Eman Abdulsemed <emanabdulsemed4398@gmail.com>
2026-07-29 19:23:31 +0300

    Add files via upload

--- B_Also_Try_Minecraft.py
+++ B_Also_Try_Minecraft.py
@@ -1,47 +1,18 @@
-import sys
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+forward = [0] * n
+for i in range(1, n):
+    forward[i] = forward[i - 1] + max(0, a[i - 1] - a[i])
+backward = [0] * n
+for i in range(n - 2, -1, -1):
+    backward[i] = backward[i + 1] + max(0, a[i + 1] - a[i])
 
-def solve():
-    
-    input = sys.stdin.read
-    data = input().split()
-    
-    if not data:
-        return
+for _ in range(m):
+    s, t = map(int, input().split())
+    s -= 1
+    t -= 1
 
-    n = int(data[0])
-    m = int(data[1])
-    
-    a = [0] + [int(x) for x in data[2:n+2]]
-    
-    
-    pref = [0] * (n + 1)
-    for i in range(2, n + 1):
-        damage = max(0, a[i-1] - a[i])
-        pref[i] = pref[i-1] + damage
-        
-     
-    suff = [0] * (n + 2)
-    for i in range(n - 1, 0, -1):
-        damage = max(0, a[i+1] - a[i])
-        suff[i] = suff[i+1] + damage
-
-    
-    idx = n + 2
-    output = []
-    for _ in range(m):
-        s = int(data[idx])
-        t = int(data[idx+1])
-        idx += 2
-        
-        if s < t:
-            
-            output.append(str(pref[t] - pref[s]))
-        else:
-            
-            output.append(str(suff[t] - suff[s]))
-            
-
-    sys.stdout.write('\n'.join(output) + '\n')
-
-if __name__ == '__main__':
-    solve()
+    if s < t:
+        print(forward[t] - forward[s])
+    else:
+        print(backward[t] - backward[s])
